9351d80ab3307a1ace3f7ed9eae2264134ce830e
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2020-05-01 12:24:56 +0530

    added add_to_end function in list.c

--- list.c
+++ list.c
@@ -12,6 +12,33 @@ List_ptr create_list(void)
   return list;
 }
 
+Node_ptr create_node(int value)
+{
+  Node_ptr new_node = malloc(sizeof(Node));
+  new_node->value = value;
+  new_node->next = NULL;
+
+  return new_node;
+}
+
+Status add_to_end(List_ptr list, int value)
+{
+  Status status ;
+  Node_ptr new_node = create_node(value);
+  if (list->head == NULL)
+  {
+    list->head = new_node;
+  }
+  else
+  {
+    list->last->next = new_node;
+  }
+  list->last = new_node;
+  list->count++;
+  status = Success;
+  return status;
+}
+
 void display(List_ptr list)
 {
   Node_ptr p_walk = list->head;
--- numbers.c
+++ numbers.c
@@ -1,8 +1,38 @@
+#include <stdio.h>
 #include "list.h"
+
+void take_operation_to_perform(char *operation)
+  {
+   printf("Please enter the alphabet of the operation you would like to perform\n");
+   scanf("%c",operation);
+  }
+
+void take_input(int *value)
+{
+  printf("Please enter a value...\n");
+  scanf("%d",value);
+}
+
+Status choose_operation(List_ptr list)
+{
+ char operation;
+ int value;
+ take_operation_to_perform(&operation);
+ switch (operation)
+  {
+  case 'a':
+    take_input(&value);
+    return add_to_end(list, value);
+    break;
+    default:
+    break;
+  }
+}
 
 int main(void)
 {
   List_ptr list = create_list();
-  display(list);
+  Status status = choose_operation(list);
+  status = 1 ? printf("Success\n") : printf("failure\n");
   return 0;
 }
