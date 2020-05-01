5651c68b806970de80154a35a414dc816c576435
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2020-05-01 16:38:14 +0530

    connected to main and now after operation it will return to the menu

--- numbers.c
+++ numbers.c
@@ -5,21 +5,24 @@ void take_operation_to_perform(char *operation)
   {
    printf("Please enter the alphabet of the operation you would like to perform\n");
    scanf("%c",operation);
+   while ((getchar()) != '\n'); 
   }
 
 void take_input(int *value)
 {
   printf("Please enter a value...\n");
   scanf("%d",value);
+  while ((getchar()) != '\n'); 
 }
 
 Status choose_operation(List_ptr list)
 {
  char operation;
- int value;
  take_operation_to_perform(&operation);
+
  switch (operation)
   {
+  int value;
   case 'a':
     take_input(&value);
     return add_to_end(list, value);
@@ -30,6 +33,8 @@ Status choose_operation(List_ptr list)
      break;
   case 'l':
      display(list);
+     Status status = 1;
+     return status;
      break;
   }
 }
@@ -37,7 +42,12 @@ Status choose_operation(List_ptr list)
 int main(void)
 {
   List_ptr list = create_list();
-  Status status = choose_operation(list);
-  status = 1 ? printf("Success\n") : printf("failure\n");
+  Status status = 1;
+  while(1)
+  {
+  status = choose_operation(list);
+  printf("Success\n");
+  }
+  printf("Failure\n");
   return 0;
 }
